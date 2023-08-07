75eeb0b4b78c24e2dd54167a350d895dd0b39eb7
André Scheiermann <scheiermann.andre@gmail.com>
2023-08-07 08:10:02 +0200

    test: add tests for GroupsScreenService

--- lib/features/1_groups/domain/groups_screen_model.dart
+++ lib/features/1_groups/domain/groups_screen_model.dart
@@ -1,9 +1,11 @@
+import 'package:flutter/foundation.dart';
 import 'package:my_time/features/1_groups/1_groups.dart';
 
 /// Model for the GroupsScreen.
+@immutable
 class GroupsScreenModel {
   /// Creates a [GroupsScreenModel].
-  GroupsScreenModel({
+  const GroupsScreenModel({
     this.groups = const [],
     this.favouriteProjects = const [],
   });
@@ -13,4 +15,19 @@ class GroupsScreenModel {
 
   /// The projects of the user.
   final List<ProjectModel> favouriteProjects;
+
+  @override
+  bool operator ==(covariant GroupsScreenModel other) {
+    if (identical(this, other)) return true;
+
+    return listEquals(other.groups, groups) &&
+        listEquals(other.favouriteProjects, favouriteProjects);
+  }
+
+  @override
+  int get hashCode => groups.hashCode ^ favouriteProjects.hashCode;
+
+  @override
+  String toString() =>
+      '''GroupsScreenModel(groups: $groups, favouriteProjects: $favouriteProjects)''';
 }
---
+++ test/features/1_groups/service/groups_screen_service_test.dart
@@ -0,0 +1,71 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:my_time/features/1_groups/1_groups.dart';
+
+class MockRealmDbGroupsRepository extends Mock
+    implements RealmDbGroupsRepository {}
+
+void main() {
+  group('GroupsScreenService', () {
+    late MockRealmDbGroupsRepository groupsRepository;
+    setUp(() {
+      groupsRepository = MockRealmDbGroupsRepository();
+    });
+    GroupsScreenService makeGroupScreenService() {
+      final container = ProviderContainer(
+        overrides: [
+          deviceStorageGroupsRepositoryProvider
+              .overrideWithValue(groupsRepository),
+        ],
+      );
+      addTearDown(container.dispose);
+      return container.read(groupsScreenServiceProvider);
+    }
+
+    test('streamGroupsScreenModel emits Combined Groups and Favourite Projects',
+        () {
+      const group1 = GroupModel.factory(id: '1', name: 'Group 1');
+      const group2 = GroupModel.factory(id: '2', name: 'Group 2');
+
+      const project1 = ProjectModel.factory(id: '1', name: 'Project 1');
+      const project2 = ProjectModel.factory(id: '2', name: 'Project 2');
+      final service = makeGroupScreenService();
+      when(groupsRepository.streamGroups).thenAnswer(
+        (_) => Stream.value(
+          [
+            group1,
+            group2,
+          ],
+        ),
+      );
+      when(groupsRepository.streamFavouriteProjects).thenAnswer(
+        (_) => Stream.value(
+          [
+            project1,
+            project2,
+          ],
+        ),
+      );
+
+      expect(
+        service.streamGroupsScreenModel(),
+        emitsInOrder(
+          [
+            const GroupsScreenModel(
+              groups: [
+                group1,
+                group2,
+              ],
+              favouriteProjects: [
+                project1,
+                project2,
+              ],
+            ),
+            emitsDone,
+          ],
+        ),
+      );
+    });
+  });
+}
